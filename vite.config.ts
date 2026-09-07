1cb37d70cf5e2e1a55f332b8a4017da10c00d403
yvankemayou13-creator <yvankemayou13@gmail.com>
2026-09-07 04:51:03 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  server: {
-    host: true,
-    port: 5173,
-  },
   optimizeDeps: {
-    exclude: ['@electric-sql/pglite'],
+    exclude: ['@electric-sql/pglite']
   },
+  build: {
+    rollupOptions: {
+      external: []
+    }
+  }
 })
